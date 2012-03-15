e20a52a6a331ebc33ca73175ceb08cceb5a7503c
Andre Perkins <frodre@gmail.com>
2012-03-15 11:20:23 -0500

    first project commit

---
+++ src/com/wisc/app/PoLLandActivity.java
@@ -0,0 +1,13 @@
+package com.wisc.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PoLLandActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
